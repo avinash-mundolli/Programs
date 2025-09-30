1022f0518713337bcf4e62b8cea9255df88a7328
Avinash Mundolli <avinashmundolli@gmail.com>
2025-09-30 10:03:32 +0530

    Add program to check if a number is positive, negative, or zero

---
+++ positive_negative_check.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class positive_negative_check{
+    public static void main(String[] args){
+        Scanner var=new Scanner(System.in);
+        while(true){
+            System.out.println("Enter the Number or type 'exit' to end program");
+            String input = var.nextLine();
+            if(input.equalsIgnoreCase("exit")){
+                System.out.println("Ending the program");
+                break;
+            }try {
+                int n=Integer.parseInt(input);
+                System.out.println(n+(n==0?"not positive nither a negative":n>0?"is positive":"is negative"));
+
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid Input Try Again");
+            }
+        }
+        var.close();
+    }
+}
