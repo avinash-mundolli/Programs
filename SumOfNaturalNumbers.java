85a21e1b5501c6aa714f9ef3096ef23aa1456180
Avinash Mundolli <avinashmundolli@gmail.com>
2025-10-01 18:14:47 +0530

    updated one

--- SumOfNaturalNumbers.java
+++ SumOfNaturalNumbers.java
@@ -4,11 +4,25 @@ import java.util.Scanner;
 public class SumOfNaturalNumbers {
     public static void main(String[] args) {
         Scanner var=new Scanner(System.in);
-        int sum = 0;
-        int number=10;
-        for(int i=1;i<=number;i++){
-            sum=sum+i;
+        System.out.println("calculate the sum of N natural numbers");
+        while(true){
+            System.out.println("Enter the Number or type 'exit' to end:");
+            String input=var.nextLine();
+            if(input.equalsIgnoreCase("exit")){
+                System.out.println("Ending the program");
+                break;
+            }
+            try {
+                int num=Integer.parseInt(input);
+                int n=0;
+            for(int i=1;i<=num;i++){
+                n=n+i;
+            }
+                System.out.println("Result: "+n);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid Input \""+input+"\" Try Again");
+            }
         }
-        System.out.println(sum);
+        var.close();
     }
 }
